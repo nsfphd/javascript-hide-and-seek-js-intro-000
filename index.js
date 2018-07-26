3330689debabdb9e0cfcacdea13940c51b09f7fa
Noelle Foster <uufarmgirl@gmail.com>
2018-07-26 16:22:16 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
 
+function nestedTarget(){
+  return document.querySelector('#nested .target')
+}
+
+function increaseRankBy(n){
+   const rankedLists=document.querySelectorAll('.ranked-list')
+
+  for (var i = 0; i < rankedLists.length; i++) {
+    var next= rankedLists[i].children
+    for (var v = 0; v<next.length; v++){
+      next[v].innerHTML=parseInt(next[v].innerHTML)+n
+    }
+  }
+}
+
+function deepestChild(){
+  var curr=document.getElementById('grand-node')
+  var next = curr.children[0]
+  while(next){
+    curr=next
+    next=curr.children[0]
+  }
+  return curr
+}
